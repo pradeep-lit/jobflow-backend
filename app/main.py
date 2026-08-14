428e6de9682098ac01232d4efd01b571e552eff2
Pradeep Chauhan <91078821+pradeep-lit@users.noreply.github.com>
2026-08-14 23:53:39 +0530

    feat: add /job post endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,48 @@
-from fastapi import FastAPI
+import itertools
+from typing import TypedDict
 
-app = FastAPI(title='Jobflow Backend', description='Backend service for aggregating and tracking job opportunities.', version='0.1.0')
+from fastapi import FastAPI, status
+from pydantic import BaseModel, Field, HttpUrl
 
-@app.get('/health')
-def health_check() -> dict[str,str]:
+app = FastAPI(
+    title="Jobflow Backend",
+    description="Backend service for aggregating and tracking job opportunities.",
+    version="0.1.0",
+)
+
+
+@app.get("/health")
+def health_check() -> dict[str, str]:
     """Return the current health of the API Service"""
-    return {'status':'ok'}
+    return {"status": "ok"}
 
+
+class JobModel(BaseModel):
+    title: str = Field(min_length=2, max_length=120)
+    company: str = Field(min_length=2, max_length=120)
+    url: HttpUrl
+    location: str | None = None
+    source: str = Field(min_length=2, max_length=50)
+
+
+class JobReturnModel(TypedDict):
+    id: int
+    title: str
+    company: str
+    url: HttpUrl
+    location: str | None
+    source: str
+
+
+# id = 0
+@app.post("/job", status_code=status.HTTP_201_CREATED)
+def create_job(job: JobModel) -> JobReturnModel:
+    
+    return {
+        "id": 1,
+        "title": job.title,
+        "company": job.company,
+        "url": job.url,
+        "location": job.location,
+        "source": job.source,
+    }
